fdf9fdfee7dc7cb750d3d24a92f115b4eedbbc26
user <username@email.com>
2024-01-11 20:30:39 +0545

    xml title shown

---
+++ lib/services/get_feed.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:html_unescape/html_unescape.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart';
+
+class GetFeed with ChangeNotifier {
+  final List<String> listVal = [];
+
+  final HtmlUnescape htmlUnescape = HtmlUnescape();
+
+  final xmlUrl =
+      Uri.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
+
+  void getData() async {
+    final response = await http.get(xmlUrl);
+
+    if (response.statusCode == 200) {
+      final xmlValue = XmlDocument.parse(response.body);
+
+      final elements = xmlValue.findAllElements("item");
+
+      for (final elem in elements.toList()) {
+        final values = elem.findAllElements("title");
+        for (final val in values.toList()) {
+          String outputString =
+              val.toString().replaceAll(RegExp(r'<title>|<\/title>'), '');
+          // String output = htmlUnescape.convert(outputString);
+          String output =
+              utf8.decode(latin1.encode(htmlEntityDecode(outputString)));
+          listVal.add(output);
+          notifyListeners();
+
+          debugPrint(output);
+        }
+      }
+
+      // debugPrint(xmlValue.toString());
+    } else {
+      debugPrint("error happened");
+    }
+  }
+}
+
+String htmlEntityDecode(String input) {
+  final text = input.replaceAllMapped(RegExp(r'&#(\d+);'),
+      (match) => String.fromCharCode(int.parse(match.group(1)!)));
+  return text.replaceAllMapped(RegExp(r'&#x([0-9A-Fa-f]+);'),
+      (match) => String.fromCharCode(int.parse(match.group(1)!, radix: 16)));
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:that_app/services/get_feed.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GetFeed getFeed = GetFeed();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("That App"),
+      ),
+      body: ListenableBuilder(
+        listenable: getFeed,
+        builder: (context, child) {
+          return Column(
+            children: [
+              IconButton(
+                onPressed: () {
+                  getFeed.getData();
+                },
+                icon: const Icon(Icons.add),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: getFeed.listVal.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(getFeed.listVal[index]),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
