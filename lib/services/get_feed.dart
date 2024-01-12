e81270c76d27ba38f089b1f6620f8f3668d116f4
user <username@email.com>
2024-01-12 21:19:01 +0545

    used getAttribute instead of complicated regex

--- lib/services/get_feed.dart
+++ lib/services/get_feed.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:html_unescape/html_unescape.dart';
 import 'package:http/http.dart' as http;
 import 'package:that_app/models/news_class.dart';
 import 'package:xml/xml.dart';
@@ -11,7 +10,7 @@ class GetFeed with ChangeNotifier {
 
   final List<String> listImg = [];
 
-  final HtmlUnescape htmlUnescape = HtmlUnescape();
+
 
   final xmlUrl =
       Uri.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
@@ -26,16 +25,9 @@ class GetFeed with ChangeNotifier {
 
       for (final elem in elements.toList()) {
         // TODO Title
-        final values = elem.findElements("title");
-
-        // Only one title inside "item". So first works. toString() without first returns items inside brackets
-        final val = values.first.toString();
-        String outputString = val.replaceAll(RegExp(r'<title>|<\/title>'), '');
-
-        String output =
-            utf8.decode(latin1.encode(htmlUnescape.convert(outputString)));
-        listVal.add(output);
-        notifyListeners();
+        final title = elem.getAttribute("title");
+        listVal.add(title.toString());
+        
 
         // debugPrint(output);
 
@@ -58,6 +50,9 @@ class GetFeed with ChangeNotifier {
         //   imageLink: imageLink,
         //   imageContent: imageContent,
         // );
+
+
+        notifyListeners(); // Important
       }
 
       // debugPrint(xmlValue.toString());
@@ -74,45 +69,8 @@ class GetFeed with ChangeNotifier {
 //       (match) => String.fromCharCode(int.parse(match.group(1)!, radix: 16)));
 // }
 
-// TODO
-extension StringXML on String {
-  String convertTitle() {
-    final replaceTagsString = replaceAll(RegExp(r'<title>|<\/title>'), '');
-    final decodedTitle =
-        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
-    return decodedTitle;
-  }
 
-  String convertLink() {
-    final replaceTagsString = replaceAll(RegExp(r'<link>|<\/link>'), '');
-    // final decodedTitle =
-    //     utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
-    return replaceTagsString;
-  }
 
-  String convertDescription() {
-    final replaceTagsString =
-        replaceAll(RegExp(r'<description>|<\/description>'), '');
-    final decodedTitle =
-        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
-    return decodedTitle;
-  }
 
-  String convertPubDate() {
-    final replaceTagsString = replaceAll(RegExp(r'<pubDate>|<\/pubDate>'), '');
-    // final decodedTitle =
-    //     utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
-    return replaceTagsString;
-  }
 
-  // TODO
-  String convertContent() {
-    final replaceTagsString =
-        replaceAll(RegExp(r'<content:encoded>|<\/content:encoded>'), '');
-    final decodedTitle =
-        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
-    return decodedTitle;
-  }
-
-  // TODO Image link and ImageSource
-}
+// TODO extension if needed
