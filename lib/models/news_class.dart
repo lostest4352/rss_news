c04d943dea9b90bfb184ed642ba6afc9472bd1c1
user <username@email.com>
2024-01-12 20:57:58 +0545

    added some code but commented for now

--- lib/models/news_class.dart
+++ lib/models/news_class.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
+
 class NewsClass {
-  
+  final String title;
+  final String? link;
+  final String? description;
+  final String? pubDate;
+  final String? content;
+  final String? imageLink;
+  final Image? imageContent;
+
+  NewsClass({
+    required this.title,
+    required this.link,
+    required this.description,
+    required this.pubDate,
+    required this.content,
+    required this.imageLink,
+    required this.imageContent,
+  });
 }
--- lib/services/get_feed.dart
+++ lib/services/get_feed.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:html_unescape/html_unescape.dart';
 import 'package:http/http.dart' as http;
+import 'package:that_app/models/news_class.dart';
 import 'package:xml/xml.dart';
 
 class GetFeed with ChangeNotifier {
@@ -34,6 +35,17 @@ class GetFeed with ChangeNotifier {
         notifyListeners();
 
         debugPrint(output);
+
+        //
+        // final NewsClass newsClass = NewsClass(
+        //   title: output,
+        //   link: link,
+        //   description: description,
+        //   pubDate: pubDate,
+        //   content: content,
+        //   imageLink: imageLink,
+        //   imageContent: imageContent,
+        // );
       }
 
       // debugPrint(xmlValue.toString());
@@ -49,3 +61,48 @@ class GetFeed with ChangeNotifier {
 //   return text.replaceAllMapped(RegExp(r'&#x([0-9A-Fa-f]+);'),
 //       (match) => String.fromCharCode(int.parse(match.group(1)!, radix: 16)));
 // }
+
+
+
+// TODO
+extension StringXML on String {
+  String convertTitle() {
+    final replaceTagsString = replaceAll(RegExp(r'<title>|<\/title>'), '');
+    final decodedTitle =
+        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
+    return decodedTitle;
+  }
+
+  String convertLink() {
+    final replaceTagsString = replaceAll(RegExp(r'<link>|<\/link>'), '');
+    // final decodedTitle =
+    //     utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
+    return replaceTagsString;
+  }
+
+  String convertDescription() {
+    final replaceTagsString = replaceAll(RegExp(r'<description>|<\/description>'), '');
+    final decodedTitle =
+        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
+    return decodedTitle;
+  }
+
+  String convertPubDate() {
+    final replaceTagsString = replaceAll(RegExp(r'<pubDate>|<\/pubDate>'), '');
+    // final decodedTitle =
+    //     utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
+    return replaceTagsString;
+  }
+
+  // TODO
+  String convertContent() {
+    final replaceTagsString = replaceAll(RegExp(r'<content:encoded>|<\/content:encoded>'), '');
+    final decodedTitle =
+        utf8.decode(latin1.encode(HtmlUnescape().convert(replaceTagsString)));
+    return decodedTitle;
+  }
+
+  // TODO Image link and ImageSource
+
+  
+}
