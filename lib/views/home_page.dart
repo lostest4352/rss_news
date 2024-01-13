39d2ea81dd934a9ba2aba6077812fbda82ccece0
user <username@email.com>
2024-01-13 19:41:22 +0545

    added selectable region and removed selectable text

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -34,44 +34,48 @@ class _HomePageState extends State<HomePage> {
                   itemCount: getFeed.newsClassList.length,
                   itemBuilder: (context, index) {
                     final classList = getFeed.newsClassList;
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: SelectableText(classList[index].title),
-                          subtitle: Builder(
-                            builder: (context) {
-                              if (classList[index].description != null) {
-                                return SelectableText(
-                                    classList[index].description!);
-                              } else {
-                                return const Center();
-                              }
-                            },
+                    return SelectableRegion(
+                      focusNode: FocusNode(),
+                      selectionControls: DesktopTextSelectionControls(),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: Text(classList[index].title),
+                            subtitle: Builder(
+                              builder: (context) {
+                                if (classList[index].description != null) {
+                                  return Text(
+                                      classList[index].description!);
+                                } else {
+                                  return const Center();
+                                }
+                              },
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Builder(
-                            builder: (context) {
-                              final imgLink = classList[index].innerImageLink;
-                              if (imgLink != null) {
-                                return CachedNetworkImage(
-                                  imageUrl: imgLink,
-                                  placeholder: (context, url) =>
-                                      const CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) {
-                                    return const Icon(Icons.error);
-                                  },
-                                );
-                              } else {
-                                return const Center(
-                                  child: Icon(Icons.newspaper),
-                                );
-                              }
-                            },
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Builder(
+                              builder: (context) {
+                                final imgLink = classList[index].innerImageLink;
+                                if (imgLink != null) {
+                                  return CachedNetworkImage(
+                                    imageUrl: imgLink,
+                                    placeholder: (context, url) =>
+                                        const CircularProgressIndicator(),
+                                    errorWidget: (context, url, error) {
+                                      return const Icon(Icons.error);
+                                    },
+                                  );
+                                } else {
+                                  return const Center(
+                                    child: Icon(Icons.newspaper),
+                                  );
+                                }
+                              },
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     );
                     // return Image.network(getFeed.listImg[index]);
                   },
